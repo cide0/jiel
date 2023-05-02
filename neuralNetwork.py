1328ca6ea2c979cb624522bceff7c28052d3965b
c_i_d_e <jace-64@gmx.de>
2023-05-02 12:49:53 +0200

    a1

---
+++ neuralNetwork.py
@@ -0,0 +1,17 @@
+class NeuralNetwork:
+
+    def __init__(self):
+        pass
+
+    def train(self):
+        print('I am training')
+
+    def query(self):
+        print('I ask..')
+
+
+
+
+
+if __name__ == '__main__':
+    neuralNetwork = NeuralNetwork()
