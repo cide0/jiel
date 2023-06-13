6b1ac40b0f5d41bcf3fb947dac7782751d4f8849
c_i_d_e <jace-64@gmx.de>
2023-06-13 12:27:39 +0200

    finished module 2.2.2

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -12,12 +12,45 @@ class NeuralNetwork:
         self.lRate = learningRate
         self.wih = (numpy.random.rand(self.hNodes, self.iNodes) - 0.5)
         self.who = (numpy.random.rand(self.oNodes, self.hNodes) - 0.5)
+        # activation function is the sigmoid function
+        self.activation_function = lambda x: scipy.special.expit(x)
 
-    def train(self):
-        print('I am training')
+    def train(self, inputs_list, targets_list):
+        print("I startet training ...")
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
 
-    def query(self):
-        print('I ask..')
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        # Some debuginformations
+        print("Final output:")
+        print(final_outputs)
+        pass
+
+    def query(self, inputs_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
 
 
 if __name__ == '__main__':
@@ -26,5 +59,10 @@ if __name__ == '__main__':
     outputNodes = 3
     learningRate = 0.5
     neuralNetwork = NeuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
-    neuralNetwork.train()
-    neuralNetwork.query()
+    input_list = [(1.0, 0.5, 0.3), (0.4, 0.9, 0.1)]
+    target_list = [(1.0, 0.0, 0.0), (0.0, 1.0, 0.0)]
+    neuralNetwork.train(input_list, target_list)
+
+    output = neuralNetwork.query([1.0, 0.5, -1.5])
+    print("Queryoutput:")
+    print(output)
