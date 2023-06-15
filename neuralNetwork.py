8e81a5e6fd20180688b4367cd5b4fa8fbf789c03
c_i_d_e <jace-64@gmx.de>
2023-06-15 12:43:18 +0200

    mnist small data

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -10,8 +10,12 @@ class NeuralNetwork:
         self.hNodes = hiddenNodes
         self.oNodes = outputNodes
         self.lRate = learningRate
-        self.wih = (numpy.random.rand(self.hNodes, self.iNodes) - 0.5)
-        self.who = (numpy.random.rand(self.oNodes, self.hNodes) - 0.5)
+        # link weight matrices, wih and who
+        # weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
+        # w11 w21
+        # w12 w22 etc
+        self.wih = numpy.random.normal(0.0, pow(self.iNodes, -0.5), (self.hNodes, self.iNodes))
+        self.who = numpy.random.normal(0.0, pow(self.hNodes, -0.5), (self.oNodes, self.hNodes))
         # activation function is the sigmoid function
         self.activation_function = lambda x: scipy.special.expit(x)
 
@@ -31,9 +35,20 @@ class NeuralNetwork:
         # calculate the signals emerging from final output layer
         final_outputs = self.activation_function(final_inputs)
 
-        # Some debuginformations
-        print("Final output:")
-        print(final_outputs)
+        # output layer error is the (target - actual)
+        output_errors = targets - final_outputs
+        # hidden layer error is the output_errors, split by weights, recombined at hidden nodes
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        # update the weights for the links between the hidden and output layers
+        self.who += self.lRate * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(hidden_outputs))
+
+        # update the weights for the links between the input and hidden layers
+        self.wih += self.lRate * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs))
+
+        #debugging
+        #print("Final output:")
+        #print(final_outputs)
         pass
 
     def query(self, inputs_list):
@@ -50,19 +65,73 @@ class NeuralNetwork:
         # calculate the signals emerging from final output layer
         final_outputs = self.activation_function(final_inputs)
 
+        #print(final_outputs)
+
         return final_outputs
 
 
 if __name__ == '__main__':
-    inputNodes = 3
-    hiddenNodes = 3
-    outputNodes = 3
-    learningRate = 0.5
+    inputNodes = 784
+    hiddenNodes = 100
+    outputNodes = 10
+    learningRate = 0.3
     neuralNetwork = NeuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
-    input_list = [(1.0, 0.5, 0.3), (0.4, 0.9, 0.1)]
-    target_list = [(1.0, 0.0, 0.0), (0.0, 1.0, 0.0)]
-    neuralNetwork.train(input_list, target_list)
 
-    output = neuralNetwork.query([1.0, 0.5, -1.5])
-    print("Queryoutput:")
-    print(output)
+    training_data_file = open("mnist_dataset/mnist_train.csv", "r")
+    training_data_list = training_data_file.readlines()
+    training_data_file.close()
+
+    # epochs is the number of times the training data set is used for training
+    epochs = 2
+
+    for e in range(epochs):
+        # go through all records in the training data set
+        for record in training_data_list:
+            # split the record by the ',' commas
+            all_values = record.split(',')
+            # scale and shift the inputs
+            inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+            # create the target output values (all 0.01, except the desired label which is 0.99)
+            targets = numpy.zeros(outputNodes) + 0.01
+            # all_values[0] is the target label for this record
+            targets[int(all_values[0])] = 0.99
+            neuralNetwork.train(inputs, targets)
+            pass
+        pass
+
+    # load the mnist test data CSV file into a list
+    test_data_file = open("mnist_dataset/mnist_test.csv", "r")
+
+    test_data_list = test_data_file.readlines()
+    test_data_file.close()
+
+    # test the neural network
+    # scorecard for how well the network performs, initially empty
+    scorecard = []
+
+    # go through all the records in the test data set
+    for record in test_data_list:
+        # split the record by the ',' commas
+        all_values = record.split(',')
+        # correct answer is first value
+        correct_label = int(all_values[0])
+        # scale and shift the inputs
+        inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+        # query the network
+        outputs = neuralNetwork.query(inputs)
+        # the index of the highest value corresponds to the label
+        label = numpy.argmax(outputs)
+        # append correct or incorrect to list
+        if (label == correct_label):
+            # network's answer matches correct answer, add 1 to scorecard
+            scorecard.append(1)
+        else:
+            # network's answer doesn't match correct answer, add 0 to scorecard
+            scorecard.append(0)
+            pass
+
+        pass
+
+    # calculate the performance score, the fraction of correct answers
+    scorecard_array = numpy.asarray(scorecard)
+    print("performance = ", scorecard_array.sum() / scorecard_array.size)
